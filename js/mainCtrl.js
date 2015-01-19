d9233e272a33655e7d23d90c57f98bcc7b3ca2f4
Michael Sayer <michael.j.say@gmail.com>
2015-01-19 14:15:41 -0700

    could use improvement

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,8 +2,12 @@ var app = angular.module("friendsList");
 
 app.controller("mainCtrl", function($scope) {
 	$scope.friends = ["mark ass", "wee man", "old John", "Jonny 5"];
-});
 
-$scope.addFriend = function() {
-	$scope.friends.push($scope.inputName);
+
+	$scope.addFriends = function() {
+	  $scope.friends.push($scope.adder);
+	  $scope.adder = '';
+	};
+
+
 });
